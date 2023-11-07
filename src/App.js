fe438d7da1f1e25493c2917ba93c0cea6c40ddc5
Chloé <chloe.du.16@live.fr>
2023-11-07 12:27:29 +0100

    add footer and header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './Footer'
+import './Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Hello la piscine</h1>
-        <p>
-          Comment ca va ?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+ <Footer />
+ <Header />
+  </div>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header () {
+    return (
+        <header>
+            <h1> Mon blog</h1>
+            <nav> 
+                <ul>
+                    <li> Acceuil</li>
+                    <li> Contact </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
